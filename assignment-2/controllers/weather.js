7089507c412bc65548736c161f910e85a9affdc9
Mohamed Jeafer <mohamed.jeafer@fundmore.ai>
2022-04-21 13:56:00 -0400

    added throttle function to controller

--- assignment-2/controllers/weather.js
+++ assignment-2/controllers/weather.js
@@ -1,26 +1,62 @@
 const { getCityKeyValue, getWeatherDetails } = require("../services/weather");
 
-const getWeather = async (req, res, next) => {
-  try {
-    const cityName = req.body.cityName;
-    const cityKeyValue = await getCityKeyValue(cityName);
-    const cityForecast = await getWeatherDetails(cityKeyValue);
-    if (cityForecast.status >=200 && cityForecast.status <300){
-      res.render("weatherDetails", {
-        forecast: cityForecast,
-        cityName: cityName,
-        pageTitle: "Weather Forecast",
-        path: "weatherDetails",
-      });
-    } else {
-      console.log(cityForecast.status)
-      res.status(cityForecast.status).redirect('/weather/weatherDetailsNotFound')
-    }
-  } catch (error) {
-      console.log(error);
-      res.redirect('/weather/weatherDetailsNotFound')
-      next()
-  }
+let cityForecast;
+let cityName
+const getWeather = (req, res, next) => {
+  updateWeather(req, res, next);
 };
+
+const renderWeatherPage = (req, res) => {
+  res.render("weatherDetails", {
+    forecast: cityForecast,
+    cityName: cityName,
+    pageTitle: "Weather Forecast",
+    path: "weatherDetails",
+  });
+};
+const updateWeather = throttle(async (req, res, next) => {
+  try {
+    cityName = req.body.cityName;
+    const cityGeoCoord = await getCityKeyValue(cityName);
+    cityForecast = await getWeatherDetails(cityGeoCoord);
+    if (
+      cityForecast &&
+      cityForecast.status >= 200 &&
+      cityForecast.status < 300
+    ) {
+      renderWeatherPage(req, res);
+    }
+    // else {
+    //   res
+    //     .status(cityForecast.status)
+    //     .redirect("/weather/weatherDetailsNotFound");
+    // }
+  } catch (error) {
+    console.log(error);
+    res.redirect("/weather/weatherDetailsNotFound");
+    next();
+  }
+});
+
+function throttle(cb, delay = 20000) {
+  let shouldWait = false;
+  
+  return (req, res, next) => {
+    console.log(req.body.cityName)
+    if (shouldWait && cityName === req.body.cityName) {
+      console.log("Getting data from DB");
+      renderWeatherPage(req, res)
+      return;
+    }
+    
+    cb(req, res, next);
+    console.log("Getting data from API");
+    shouldWait = true
+    setTimeout(() => {
+      console.log("Done waiting")
+      shouldWait = false;
+    }, delay);
+  };
+}
 
 module.exports = getWeather;
