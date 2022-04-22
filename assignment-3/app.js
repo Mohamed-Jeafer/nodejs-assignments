84f3f49e983a1fd6103df9c64ec862cccc7bbe94
Mohamed Jeafer <mohamed.jeafer@fundmore.ai>
2022-04-22 13:21:45 -0400

    callback and event function added

---
+++ assignment-3/app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
---
+++ assignment-3/public/js/sum.js
@@ -0,0 +1,38 @@
+document.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    getValues(document.querySelector("button"));
+  }
+});
+// let result = 0
+const getValues = (btn) => {
+  const n1 = btn.parentNode.querySelector("[id=n1]").value;
+  const n2 = btn.parentNode.querySelector("[id=n2]").value;
+  const result = sum(sumOfMultiples(+n1), sumOfMultiples(+n2));
+  setTimeout(()=>{
+    logInfo(result)
+  }, 1000)
+};
+
+
+
+const logInfo = (sum) => {
+  const result = document.getElementById("myEvent");
+  result.textContent = sum;
+};
+
+const sumOfMultiples = (number) => {
+  let sum = 0;
+  for (let i = number; i <= 1000; i++) {
+    if (i % number === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+const sum = (multiplesOfNum1, multiplesOfNum2) => {
+  let sum = multiplesOfNum1 + multiplesOfNum2;
+  return sum;
+};
+
+//  setTimeout(()=>{}, 2000);
